0d058f1726354f8188460301c425f5a7e2c00d42
Jusong Yu <jusong.yeu@gmail.com>
2025-01-18 01:10:26 +0100

    sorted

--- src/plumpy/__init__.py
+++ src/plumpy/__init__.py
@@ -4,110 +4,153 @@ __version__ = '0.24.0'
 
 import logging
 
+from .base.state_machine import TransitionFailed
+
 # interfaces
-from .controller import *
-from .coordinator import *
-from .events import *
-from .exceptions import *
-from .futures import *
-from .loaders import *
-from .message import *
-from .mixins import *
-from .persistence import *
-from .ports import *
-from .process_listener import *
-from .process_states import *
-from .processes import *
-from .utils import *
-from .workchains import *
+from .controller import ProcessController
+from .coordinator import Coordinator
+from .events import (
+    PlumpyEventLoopPolicy,
+    get_event_loop,
+    new_event_loop,
+    reset_event_loop_policy,
+    run_until_complete,
+    set_event_loop,
+    set_event_loop_policy,
+)
+from .exceptions import (
+    ClosedError,
+    CoordinatorConnectionError,
+    CoordinatorTimeoutError,
+    InvalidStateError,
+    KilledError,
+    PersistenceError,
+    UnsuccessfulResult,
+)
+from .futures import CancellableAction, Future, capture_exceptions, create_task
+from .loaders import DefaultObjectLoader, ObjectLoader, get_object_loader, set_object_loader
+from .message import MessageBuilder, ProcessLauncher, create_continue_body, create_launch_body
+from .mixins import ContextMixin
+from .persistence import (
+    Bundle,
+    InMemoryPersister,
+    LoadSaveContext,
+    PersistedCheckpoint,
+    Persister,
+    PicklePersister,
+    Savable,
+    SavableFuture,
+    auto_persist,
+)
+from .ports import UNSPECIFIED, InputPort, OutputPort, Port, PortNamespace, PortValidationError
+from .process_listener import ProcessListener
+from .process_spec import ProcessSpec
+from .process_states import (
+    Continue,
+    Created,
+    Excepted,
+    Finished,
+    Interruption,
+    Kill,
+    Killed,
+    KillInterruption,
+    PauseInterruption,
+    ProcessState,
+    Running,
+    Stop,
+    Wait,
+    Waiting,
+)
+from .processes import BundleKeys, Process
+from .utils import AttributesDict
+from .workchains import ToContext, WorkChain, WorkChainSpec, if_, return_, while_
 
 __all__ = (
-    # controller
-    'ProcessController',
-    # coordinator
-    'Coordinator',
-    # event
-    'PlumpyEventLoopPolicy',
-    'get_event_loop',
-    'new_event_loop',
-    'reset_event_loop_policy',
-    'run_until_complete',
-    'set_event_loop',
-    'set_event_loop_policy',
-    # exceptions
-    'ClosedError',
-    'CoordinatorConnectionError',
-    'CoordinatorTimeoutError',
-    'InvalidStateError',
-    'KilledError',
-    'PersistenceError',
-    'UnsuccessfulResult',
-    # futures
-    'CancellableAction',
-    'Future',
-    'capture_exceptions',
-    'create_task',
-    'create_task',
-    # loaders
-    'DefaultObjectLoader',
-    'ObjectLoader',
-    'get_object_loader',
-    'set_object_loader',
-    # message
-    'MessageBuilder',
-    'ProcessLauncher',
-    'create_continue_body',
-    'create_launch_body',
-    # mixins
-    'ContextMixin',
-    # persistence
-    'Bundle',
-    'InMemoryPersister',
-    'LoadSaveContext',
-    'PersistedCheckpoint',
-    'Persister',
-    'PicklePersister',
-    'Savable',
-    'SavableFuture',
-    'auto_persist',
     # ports
     'UNSPECIFIED',
-    'InputPort',
-    'OutputPort',
-    'Port',
-    'PortNamespace',
-    'PortValidationError',
-    # process_listener
-    'ProcessListener',
+    # utils
+    'AttributesDict',
+    # persistence
+    'Bundle',
+    # processes
+    'BundleKeys',
+    # futures
+    'CancellableAction',
+    # exceptions
+    'ClosedError',
+    # mixins
+    'ContextMixin',
     # process_states/States
     'Continue',
+    # coordinator
+    'Coordinator',
+    'CoordinatorConnectionError',
+    'CoordinatorTimeoutError',
     'Created',
+    # loaders
+    'DefaultObjectLoader',
     'Excepted',
     'Finished',
+    'Future',
+    'InMemoryPersister',
+    'InputPort',
     'Interruption',
+    'InvalidStateError',
     # process_states/Commands
     'Kill',
     'KillInterruption',
     'Killed',
+    'KilledError',
+    'LoadSaveContext',
+    # message
+    'MessageBuilder',
+    'ObjectLoader',
+    'OutputPort',
     'PauseInterruption',
+    'PersistedCheckpoint',
+    'PersistenceError',
+    'Persister',
+    'PicklePersister',
+    # event
+    'PlumpyEventLoopPolicy',
+    'Port',
+    'PortNamespace',
+    'PortValidationError',
+    'Process',
+    # controller
+    'ProcessController',
+    'ProcessLauncher',
+    # process_listener
+    'ProcessListener',
+    'ProcessSpec',
     'ProcessState',
     'Running',
+    'Savable',
+    'SavableFuture',
     'Stop',
-    'Wait',
-    'Waiting',
-    # processes
-    'BundleKeys',
-    'Process',
-    'ProcessSpec',
-    'TransitionFailed',
-    # utils
-    'AttributesDict',
     # workchain
     'ToContext',
+    'TransitionFailed',
+    'UnsuccessfulResult',
+    'Wait',
+    'Waiting',
     'WorkChain',
     'WorkChainSpec',
+    'auto_persist',
+    'capture_exceptions',
+    'create_continue_body',
+    'create_launch_body',
+    'create_task',
+    'get_event_loop',
+    'get_object_loader',
     'if_',
+    'new_event_loop',
+    'reset_event_loop_policy',
     'return_',
+    'run_until_complete',
+    'set_event_loop',
+    'set_event_loop_policy',
+    'set_object_loader',
     'while_',
 )
 
