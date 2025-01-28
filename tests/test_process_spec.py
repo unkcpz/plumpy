3234a3fdc95a3021ddf44e27e3ceed0e0db63bb7
Jusong Yu <jusong.yeu@gmail.com>
2025-01-28 22:55:10 +0100

    - test_process_spec.py

--- tests/test_process_spec.py
+++ tests/test_process_spec.py
@@ -1,15 +1,14 @@
 # -*- coding: utf-8 -*-
 from plumpy import ProcessSpec
 from plumpy.ports import InputPort, PortNamespace
-
-from .utils import TestCase
+import unittest
 
 
 class StrSubtype(str):
     pass
 
 
-class TestProcessSpec(TestCase):
+class TestProcessSpec(unittest.TestCase):
     def setUp(self):
         self.spec = ProcessSpec()
 
@@ -20,15 +19,15 @@ class TestProcessSpec(TestCase):
         input_ports = self.spec.inputs
         output_ports = self.spec.outputs
 
-        self.assertTrue(input_ports.name, self.spec.NAME_INPUTS_PORT_NAMESPACE)
-        self.assertTrue(output_ports.name, self.spec.NAME_OUTPUTS_PORT_NAMESPACE)
+        assert input_ports.name, self.spec.NAME_INPUTS_PORT_NAMESPACE
+        assert output_ports.name, self.spec.NAME_OUTPUTS_PORT_NAMESPACE
 
     def test_dynamic_output(self):
         self.spec.outputs.dynamic = True
         self.spec.outputs.valid_type = str
-        self.assertIsNone(self.spec.outputs.validate({'dummy': 'foo'}))
-        self.assertIsNone(self.spec.outputs.validate({'dummy': StrSubtype('bar')}))
-        self.assertIsNotNone(self.spec.outputs.validate({'dummy': 5}))
+        assert self.spec.outputs.validate({'dummy': 'foo'}) is None
+        assert self.spec.outputs.validate({'dummy': StrSubtype('bar')}) is None
+        assert self.spec.outputs.validate({'dummy': 5}) is not None
 
         # Remove dynamic output
         self.spec.outputs.dynamic = False
@@ -37,9 +36,9 @@ class TestProcessSpec(TestCase):
         # Now add and check behaviour
         self.spec.outputs.dynamic = True
         self.spec.outputs.valid_type = str
-        self.assertIsNone(self.spec.outputs.validate({'dummy': 'foo'}))
-        self.assertIsNone(self.spec.outputs.validate({'dummy': StrSubtype('bar')}))
-        self.assertIsNotNone(self.spec.outputs.validate({'dummy': 5}))
+        assert self.spec.outputs.validate({'dummy': 'foo'}) is None
+        assert self.spec.outputs.validate({'dummy': StrSubtype('bar')}) is None
+        assert self.spec.outputs.validate({'dummy': 5}) is not None
 
     def test_get_description(self):
         spec = ProcessSpec()
@@ -47,13 +46,13 @@ class TestProcessSpec(TestCase):
         # Adding an input should create some description
         spec.input('test')
         description = spec.get_description()
-        self.assertNotEqual(description, {})
+        assert description != {}
 
         # Similar with adding output
         spec = ProcessSpec()
         spec.output('test')
         description = spec.get_description()
-        self.assertNotEqual(description, {})
+        assert description != {}
 
     def test_input_namespaced(self):
         """
@@ -61,15 +60,15 @@ class TestProcessSpec(TestCase):
         """
         self.spec.input('some.name.space.a', valid_type=int)
 
-        self.assertTrue('some' in self.spec.inputs)
-        self.assertTrue('name' in self.spec.inputs['some'])
-        self.assertTrue('space' in self.spec.inputs['some']['name'])
-        self.assertTrue('a' in self.spec.inputs['some']['name']['space'])
+        assert 'some' in self.spec.inputs
+        assert 'name' in self.spec.inputs['some']
+        assert 'space' in self.spec.inputs['some']['name']
+        assert 'a' in self.spec.inputs['some']['name']['space']
 
-        self.assertTrue(isinstance(self.spec.inputs.get_port('some'), PortNamespace))
-        self.assertTrue(isinstance(self.spec.inputs.get_port('some.name'), PortNamespace))
-        self.assertTrue(isinstance(self.spec.inputs.get_port('some.name.space'), PortNamespace))
-        self.assertTrue(isinstance(self.spec.inputs.get_port('some.name.space.a'), InputPort))
+        assert isinstance(self.spec.inputs.get_port('some'), PortNamespace)
+        assert isinstance(self.spec.inputs.get_port('some.name'), PortNamespace)
+        assert isinstance(self.spec.inputs.get_port('some.name.space'), PortNamespace)
+        assert isinstance(self.spec.inputs.get_port('some.name.space.a'), InputPort)
 
     def test_validator(self):
         """Test the port validator with default."""
@@ -81,14 +80,14 @@ class TestProcessSpec(TestCase):
         self.spec.input('dict', default={'key': 'value'}, validator=dict_validator)
 
         processed = self.spec.inputs.pre_process({})
-        self.assertEqual(processed, {'dict': {'key': 'value'}})
+        assert processed == {'dict': {'key': 'value'}}
         self.spec.inputs.validate()
 
         processed = self.spec.inputs.pre_process({'dict': {'key': 'value'}})
-        self.assertEqual(processed, {'dict': {'key': 'value'}})
+        assert processed == {'dict': {'key': 'value'}}
         self.spec.inputs.validate()
 
-        self.assertIsNotNone(self.spec.inputs.validate({'dict': {'wrong_key': 'value'}}))
+        assert self.spec.inputs.validate({'dict': {'wrong_key': 'value'}}) is not None
 
     def test_validate(self):
         """Test the global spec validator functionality."""
@@ -103,12 +102,12 @@ class TestProcessSpec(TestCase):
         self.spec.inputs.validator = is_valid
 
         processed = self.spec.inputs.pre_process({'a': 'a'})
-        self.assertEqual(processed, {'a': 'a'})
+        assert processed == {'a': 'a'}
         self.spec.inputs.validate()
 
         processed = self.spec.inputs.pre_process({'b': 'b'})
-        self.assertEqual(processed, {'b': 'b'})
+        assert processed == {'b': 'b'}
         self.spec.inputs.validate()
 
-        self.assertIsNotNone(self.spec.inputs.validate({}))
-        self.assertIsNotNone(self.spec.inputs.validate({'a': 'a', 'b': 'b'}))
+        assert self.spec.inputs.validate({}) is not None
+        assert self.spec.inputs.validate({'a': 'a', 'b': 'b'}) is not None
